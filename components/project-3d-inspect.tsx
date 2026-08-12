5966f8ee16ee6d66bcb13b06d211c4426f4e2408
Dilara Alkınoğlu <dilaraalkinoglu80@gmail.com>
2026-08-12 22:29:08 +0300

    gereksiz kütüphaneler kaldırıldı

--- components/project-3d-inspect.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { InteractiveGalleryViewer } from './interactive-gallery-viewer';
-
-export function Project3DInspect({ images, alt }: { images: string[]; alt: string }) {
-  return <InteractiveGalleryViewer images={images} alt={alt} />;
-}
